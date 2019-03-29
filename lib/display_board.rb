0d9af99c774414271ebb42e9b27f596f93a3d39d
Awoh Negheie <awohnegheie@gmail.com>
2019-03-29 15:03:39 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,23 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
+  Welcome to Tic Tac Toe
+     |   |   
+  -----------
+     |   |   
+  -----------
+     |   |   
+  What move would X like to make (1-9)?
+  > 5
+     |   |   
+  -----------
+     | X |   
+  -----------
+     |   |   
+  What move would O like to make (1-9)?
+  > 1
+   0 |   |   
+  -----------
+     | X |   
+  -----------
+     |   |   
+  # etc...
